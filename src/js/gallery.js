541cac0a77a378da71ab470a9df2a3db4465ac0b
Andrii <andrii.kondratovich@gmail.com>
2024-04-18 19:39:37 +0300

    UPD: dependencies

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -7,26 +7,25 @@ const refs = {
 };
 
 // * without handlebars:
-const galleryMarkup = countriesArr
-  .map(({ flag, name: countryName, capital, currency, language, population, infected }) => {
-    return `<li class="gallery__item">
-        <div class="gallery__thumb">
-          <img src="${flag}" alt="Флаг" width="320" />
-        </div>
-        <h2>${countryName}</h2>
-        <p><b>Столица: </b> ${capital}</p>
-        <p><b>Валюта:</b>${currency}</p>
-        <p><b>Официальный язык:</b>${language}</p>
-        <p><b>Население:</b> ${population} человек</p>
-        <p style="color: ${infected ? "red" : "green"};"> ${infected ? "Заражені" : "Здорові"}</p>
-      </li>
-    
-      `;
-  })
-  .join("");
+// const galleryMarkup = countriesArr
+//   .map(({ flag, name: countryName, capital, currency, language, population, infected }) => {
+//     return `<li class="gallery__item">
+//         <div class="gallery__thumb">
+//           <img src="${flag}" alt="Флаг" width="320" />
+//         </div>
+//         <h2>${countryName}</h2>
+//         <p><b>Столица: </b> ${capital}</p>
+//         <p><b>Валюта:</b>${currency}</p>
+//         <p><b>Официальный язык:</b>${language}</p>
+//         <p><b>Население:</b> ${population} человек</p>
+//         <p style="color: ${infected ? "red" : "green"};"> ${infected ? "Заражені" : "Здорові"}</p>
+//       </li>
+//       `;
+//   })
+//   .join("");
 
 // * with handlebars:
 
-// const galleryMarkup = countriesTpl(countriesArr);
+const galleryMarkup = countriesTpl(countriesArr);
 
 refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
