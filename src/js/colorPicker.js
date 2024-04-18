93da483c8ebde455c67126fa70c57ea7d472dddb
Andrii <andrii.kondratovich@gmail.com>
2024-04-18 17:58:00 +0300

    ADD: gallery handlebars

--- src/js/colorPicker.js
+++ src/js/colorPicker.js
@@ -1,30 +1,50 @@
-import colors from "../dataBases/colors.json";
+import colorsArr from "../dataBases/colors.json";
+
+import colorCardTpl from "../templates/myColorCard.hbs";
+// console.log("colorCardTpl:::", colorCardTpl(colorsArr[1]));
+
+import colorCardsTpl from "../templates/myColorCards.hbs";
+// console.log("colorCardsTpl:::", colorCardsTpl(colorsArr));
 
 const cards = document.querySelector(".js-palette");
 
-function cardsMarkupCreate(cards) {
-  return cards
-    .map(({ hex, rgb }) => {
-      return `
-      <div class="color-card">
-        <div>
-          <div>
-            <div>
-              <div class="color-swatch" data-hex="${hex}" data-rgb="${rgb}" style="background-color: ${hex}"></div>
-            </div>
-          </div>
-        </div>
-        <div class="color-meta">
-          <p>HEX: ${hex}</p>
-          <p>RGB: ${rgb}</p>
-        </div>
-      </div>
-    `;
-    })
-    .join("");
+// * without handlebars:
+// function cardsMarkupCreate(arr) {
+//   return arr
+//     .map(({ hex, rgb }) => {
+//       return `
+//       <div class="color-card">
+//         <div>
+//           <div>
+//             <div>
+//               <div class="color-swatch" data-hex="${hex}" data-rgb="${rgb}" style="background-color: ${hex}"></div>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="color-meta">
+//           <p>HEX: ${hex}</p>
+//           <p>RGB: ${rgb}</p>
+//         </div>
+//       </div>
+//     `;
+//     })
+//     .join("");
+// }
+
+// * with handlebars - one card (one object in array)
+// function cardsMarkupCreate(arr) {
+//   // або:
+//   // return arr.map(colorCard => colorCardTpl(colorCard)).join("");
+//   // або:
+//   return arr.map(colorCardTpl).join("");
+// }
+
+// * with handlebars - many cards (array of objects)
+function cardsMarkupCreate(arr) {
+  return colorCardsTpl(arr);
 }
 
-const cardsGalleryMarkup = cardsMarkupCreate(colors);
+const cardsGalleryMarkup = cardsMarkupCreate(colorsArr);
 cards.insertAdjacentHTML("beforeend", cardsGalleryMarkup);
 
 cards.addEventListener("click", onClickCard);
---
+++ src/js/gallery.js
@@ -0,0 +1,29 @@
+import countriesArr from "../dataBases/countries.json";
+
+import countriesTpl from "../templates/myCountries.hbs";
+
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+};
+
+// * without handlebars:
+// const galleryMarkup = countriesArr
+//   .map(({ flag, name: countryName, capital, currency, language, population }) => {
+//     return `<li class="gallery__item">
+//         <div class="gallery__thumb">
+//           <img src="${flag}" alt="Флаг" width="320" />
+//         </div>
+//         <h2>${countryName}</h2>
+//         <p><b>Столица: </b> ${capital}</p>
+//         <p><b>Валюта:</b>${currency}</p>
+//         <p><b>Официальный язык:</b>${language}</p>
+//         <p><b>Население:</b> ${population} человек</p>
+//       </li>`;
+//   })
+//   .join("");
+
+// * with handlebars:
+
+const galleryMarkup = countriesTpl(countriesArr);
+
+refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
